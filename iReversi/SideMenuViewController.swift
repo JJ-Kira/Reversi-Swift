827fd2c6decf191fb2ea84ff446d2a056a99d704
Julia Szczuczko <julia@adcon.pl>
2024-09-12 00:38:31 +0200

    side menu preparation

---
+++ iReversi/SideMenuViewController.swift
@@ -0,0 +1,14 @@
+//
+//  SideMenuViewController.swift
+//  iReversi
+//
+//  Created by Julia Szczuczko on 11/09/2024.
+//
+
+import Foundation
+
+import UIKit
+
+class SideMenuViewController: UIViewController {
+    
+}
