4f703295a9feab2dd8fb2e7cfd1ef8f5a20af895
Julia Szczuczko <julia@adcon.pl>
2024-09-18 20:10:56 +0200

    working side menu

---
+++ iReversi/App/SideMenuViewController.swift
@@ -0,0 +1,87 @@
+//
+//  SideMenuViewController.swift
+//  iReversi
+//
+//  Created by Julia Szczuczkok on 18/09/2024.
+//
+import UIKit
+
+protocol SideMenuViewControllerDelegate {
+    func selectedCell(_ row: Int)
+}
+
+class SideMenuViewController: UIViewController {
+    @IBOutlet var headerImageView: UIImageView!
+    @IBOutlet var sideMenuTableView: UITableView!
+    @IBOutlet var footerLabel: UILabel!
+
+    var delegate: SideMenuViewControllerDelegate?
+
+    var defaultHighlightedCell: Int = 0
+
+    var menu: [SideMenuModel] = [
+        SideMenuModel(icon: UIImage(named: "human")!, title: "New 2-Player Game"),
+        SideMenuModel(icon: UIImage(named: "ABP")!, title: "New ABP Game"),
+        SideMenuModel(icon: UIImage(named: "MCTS")!, title: "New MCTS Game"),
+        SideMenuModel(icon: UIImage(named: "quit")!, title: "Quit"),
+    ]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // TableView
+        self.sideMenuTableView.delegate = self
+        self.sideMenuTableView.dataSource = self
+        self.sideMenuTableView.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        self.sideMenuTableView.separatorStyle = .none
+
+        // Set Highlighted Cell
+        DispatchQueue.main.async {
+            let defaultRow = IndexPath(row: self.defaultHighlightedCell, section: 0)
+            self.sideMenuTableView.selectRow(at: defaultRow, animated: false, scrollPosition: .none)
+        }
+
+        // Footer
+        self.footerLabel.textColor = UIColor.white
+        self.footerLabel.font = UIFont.systemFont(ofSize: 12, weight: .bold)
+        self.footerLabel.text = "by Julia Szczuczko"
+
+        // Register TableView Cell
+        self.sideMenuTableView.register(SideMenuCell.nib, forCellReuseIdentifier: SideMenuCell.identifier)
+
+        // Update TableView with the data
+        self.sideMenuTableView.reloadData()
+    }
+}
+
+extension SideMenuViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 44
+    }
+}
+
+// MARK: - UITableViewDataSource
+
+extension SideMenuViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.menu.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: SideMenuCell.identifier, for: indexPath) as? SideMenuCell else { fatalError("xib doesn't exist") }
+
+        cell.iconImageView.image = self.menu[indexPath.row].icon
+        cell.titleLabel.text = self.menu[indexPath.row].title
+
+        // Highlighted color
+        let myCustomSelectionColorView = UIView()
+        myCustomSelectionColorView.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        cell.selectedBackgroundView = myCustomSelectionColorView
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        self.delegate?.selectedCell(indexPath.row)
+    }
+}
+
